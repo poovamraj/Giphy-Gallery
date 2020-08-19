229c011e48db08b3fc44f4407de1113f4fd211cc
Poovamraj T T <poovamraj@gmail.com>
2020-08-19 17:50:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/poovam/giphygallery/GiphyGalleryApplication.kt
@@ -0,0 +1,9 @@
+package com.poovam.giphygallery
+
+import android.app.Application
+
+class GiphyGalleryApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/poovam/giphygallery/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.poovam.giphygallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.flow.flow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
